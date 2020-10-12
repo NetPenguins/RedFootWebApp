abde59cdae194d8ef97b7c5697a0b5928f975119
NetPenguins <cwils192@uncc.edu>
2020-10-11 22:09:12 -0400

    added form testing for netlify forms

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,18 @@ const About = () => {
   return (
     <Layout>
       <SEO title="About" />
+      <form style={{paddingTop: '10rem'}} name="contact" netlify netlify-honeypot data-netlify="true" data-netlify-recaptcha="true">
+        <p>
+          <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Email <input type="email" name="email" /></label>
+        </p>
+        <div data-netlify-recaptcha="true"/>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </Layout>
   )
 }
